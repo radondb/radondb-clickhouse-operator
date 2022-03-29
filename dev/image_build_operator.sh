72569024aa894341e3c2ff07c99e69176174ffcc
hhhhhzhen <suhouzhen2020@gmail.com>
2022-03-29 11:01:59 +0800

    modify build shell script

---
+++ dev/image_build_operator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+SRC_ROOT="$(realpath "${CUR_DIR}/..")"
+
+# Externally configurable build-dependent options
+TAG="${TAG:-latest}"
+IMAGE_PREFIX="${IMAGE_PREFIX:-radondb}"
+
+DOCKER_IMAGE="${IMAGE_PREFIX}/clickhouse-operator:${TAG}"
+DOCKERFILE_DIR="${SRC_ROOT}/dockerfile/operator"
+
+source "${CUR_DIR}/image_build_universal.sh"
